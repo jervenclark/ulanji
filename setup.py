fbdc5658c32fdd4742a21b5f5260daba88fd1f94
Jerven Clark Chua <jervenclark@gmail.com>
2020-07-12 17:00:54 +0800

    Include non-python code on setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ulanji", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Jerven Clark Chua",
     author_email="jervenclark@gmail.com",
     description="Distraction free markdown editor",
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jervenclark/ulanji",
     packages=setuptools.find_packages(),
+    package_data={
+        'ulanji.gui': ['*.ui']
+    },
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
